3d48775f4b2185848b212a1b155acf57d55a37e2
nnawar706 <86843663+nnawar706@users.noreply.github.com>
2022-09-02 19:56:21 +0600

    Add files via upload

---
+++ fostercare/paws/assets/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    var autoplaySlider = $('#autoplay').lightSlider({
+        auto:true,
+        loop:true,
+        pauseOnHover: true,
+        onBeforeSlide: function (el) {
+            $('#current').text(el.getCurrentSlideCount());
+        } 
+    });
+    $('#total').text(autoplaySlider.getTotalSlideCount());
+});
+
+$(document).ready(function() {
+    $('#responsive').lightSlider({
+        item:3,
+        loop:false,
+        slideMove:1,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed:600,
+        responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    item:2,
+                    slideMove:1,
+                    slideMargin:6,
+                  }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:1,
+                    slideMove:1
+                  }
+            }
+        ]
+    });  
+  });
